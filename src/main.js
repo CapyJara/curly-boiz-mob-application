eccfbbdff1ce5812d29c4d1416bf827c14bd075c
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-04 14:00:29 -0800

    pre mob build

--- src/main.js
+++ src/main.js
@@ -1,35 +1,48 @@
 const submit = document.getElementById('form');
+const curlyRange = document.getElementById('curliness');
+const averageCurl = document.getElementById('average-curl');
 
+curlyRange.addEventListener('change', function() { 
+    averageCurl.textContent = curlyRange.value;
+});
 
-submit.addEventListener('submit', function(event) {
+submit.addEventListener('submit', function() {
     event.preventDefault();
-    console.log('its working');
-    
+ 
     const name = submit.elements.name.value;
     const street = submit.elements.street.value;
     const city = submit.elements.city.value;
     const state = submit.elements.state.value;
-    const keys = submit.elements.key.value;
-    
-    
-    // pizza time
+    const key = submit.elements.key.value;
+ 
     const pizza = submit.elements.toppings;
+    const pizzaPreference = [];
+ 
     for(let i = 0; i < pizza.length; i++){
         const toppingChoices = pizza[i];
         if(toppingChoices.checked) {
-            console.log(toppingChoices.value);
+            pizzaPreference[i] = toppingChoices.value;
         }
     }
-
-
+ 
+    const appSubmission = {
+        name: name,
+        street: street,
+        city: city,
+        state: state,
+        key: key,
+        pizza: pizzaPreference,
+        curl: curlyRange.value
+    };
+ 
+    console.log(appSubmission);
 });
+
+
+
+
         // form incomplete checker
         // read input
-            // console logging
-                // name
-                // address
-                // key location
-                // pizza toppings
         // evaluate pineapple conformity
             // take care of them
             // in the street fraternity
